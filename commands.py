e5928dd4f30677376314b72ba344459323fe4222
Артур Абдрахманов <a.arthur77777@gmail.com>
2023-03-11 21:31:43 +0300

    Добавил команду создания пользователя, если пользователей в бд нет

--- commands.py
+++ commands.py
@@ -1,7 +1,10 @@
 import click
 
 from database import Base, SessionLocal
-from src.models import *
+# необходимо для того, чтобы при удалении/инициализации бд было видно все таблицы
+from src.models import *  # noqa
+from src.queries import user as user_queries
+from src.schemas import UserCreateSchema
 
 
 @click.group()
@@ -21,6 +24,23 @@ def init_db():
     session = SessionLocal()
     Base.metadata.create_all(session.connection().engine)
     print('Database is created!')
+
+
+@cli.command()
+def create_default_user():
+    session = SessionLocal()
+    all_users = user_queries.get_all_users(db=session, limit=1, skip=0)
+    if not all_users:
+        user_schema = UserCreateSchema(
+            username='admin',
+            password='password',
+            password_repeat='password',
+        )
+        user_queries.create_user(db=session, user_schema=user_schema)
+
+        print(f'Default user has been created!\nlogin: {user_schema.username}\npassword: {user_schema.password}')
+    else:
+        print('There are users in database already! Not creating another one.')
 
 
 if __name__ == '__main__':
